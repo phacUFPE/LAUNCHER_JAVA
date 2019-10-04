c7d11ba03ded8b91e6578b599eb0e51182c2fb86
Pedro Cruz <phac@cin.ufpe.br>
2019-10-03 22:25:44 -0300

    Updated LoadWindow

--- src/download/LoadWindow.java
+++ src/download/LoadWindow.java
@@ -7,6 +7,14 @@ import java.awt.Font;
 import java.awt.Toolkit;
 import java.awt.Window;
 import java.awt.Window.Type;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -16,39 +24,43 @@ public class LoadWindow {
 
 	private JFrame frame;
 	
+	private static LoadWindow window;
+	
 	static JLabel lblLoadingLanguages = new JLabel();
 
 	/**
 	 * Launch the application.
+	 * @throws InterruptedException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					LoadWindow window = new LoadWindow();
+					window = new LoadWindow();
 					window.frame.setVisible(true);
-					
-					lblLoadingLanguages.setText("Loading Languages...");
-					MainWindow.languages = Language.loadLanguages();
-					lblLoadingLanguages.setText("Getting Server Version...");
-					MainWindow.serverVer = Version.getServer(MainWindow.webAddress, MainWindow.fileVersion, "current_ver");
-					MainWindow.serverMinVer = Version.getServer(MainWindow.webAddress, MainWindow.fileVersion, "min_ver");
-					lblLoadingLanguages.setText("Getting Local Version...");
-					MainWindow.localVer = Version.getLocal(MainWindow.fileConfig);
-					
-					MainWindow.getWebFiles();
-					
-					MainWindow mWin = new MainWindow();
-					
-					window.frame.dispose();
-					
-					mWin.getFrame().setVisible(true);
-					
+
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
+					
+		lblLoadingLanguages.setText("Loading Languages...");
+		MainWindow.languages = Language.loadLanguages();
+		lblLoadingLanguages.setText("Retrieving Server Version...");
+		MainWindow.serverVer = Version.getServer(MainWindow.webAddress, MainWindow.fileVersion, "current_ver");
+		MainWindow.serverMinVer = Version.getServer(MainWindow.webAddress, MainWindow.fileVersion, "min_ver");
+		lblLoadingLanguages.setText("Retrieving Local Version...");
+		MainWindow.localVer = Version.getLocal(MainWindow.fileConfig);
+					
+		lblLoadingLanguages.setText("Retrieving Files...");
+		getWebFiles(MainWindow.webAddress + MainWindow.hashList);
+					
+		MainWindow mWin = new MainWindow();
+					
+		window.frame.dispose();
+					
+		mWin.getFrame().setVisible(true);
 	}
 
 	/**
@@ -71,6 +83,7 @@ public class LoadWindow {
 		frame.setBounds(100, 100, 450, 350);
 		frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
 		frame.getContentPane().setLayout(null);
+		frame.setBackground(Color.BLACK);
 		
 		lblLoadingLanguages.setForeground(Color.WHITE);
 		lblLoadingLanguages.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 16));
@@ -86,5 +99,39 @@ public class LoadWindow {
 	public Window getFrame() {
 		// TODO Auto-generated method stub
 		return frame;
+	}
+	
+	public static void getWebFiles(String address) {
+		try {
+			
+			HashMap<String, String> hm = new HashMap<String, String>();
+			
+			URL url = new URL(address);
+			Scanner s = new Scanner(url.openStream());
+		   
+			String text = "";
+		   
+			while (s.hasNextLine()) {
+				text += s.nextLine() + "\n";
+			}
+			s.close();
+		   
+			Pattern pat = Pattern.compile("file=\"(.*)\", hash=\"(.*)\";");
+			Matcher mat = pat.matcher(text);
+			while (mat.find()) {
+			   	hm.put(mat.group(1), mat.group(2));
+			}
+			
+			MainWindow.pathMD5files = hm;
+		   
+			pat = Pattern.compile("totalfiles=(.*)");
+			mat = pat.matcher(text);
+			if (mat.find()) {
+				MainWindow.totalFiles = Integer.parseInt(mat.group(1));
+			}
+		   
+		} catch(Exception e) {
+			ErrorLog.saveError(e);
+		}
 	}
 }
