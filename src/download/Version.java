e58a419e3f36c05505608cd695520a79019f58dc
Pedro Cruz <phac@cin.ufpe.br>
2019-10-06 02:44:51 -0300

    Added method WriteLocal and Changed return null to "0"

--- src/download/Version.java
+++ src/download/Version.java
@@ -1,6 +1,8 @@
 package download;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.net.URL;
 import java.util.Scanner;
 import java.util.regex.*;
@@ -8,13 +10,14 @@ import java.util.regex.*;
 public class Version {
 	
 	public static boolean compareSL(String sVer, String lVer) {
+		if (sVer == null || lVer == null) { return false; }
 		return (sVer.compareTo(lVer) == 0);
 	}
 	
-	public static String getServer(String webAddr, String fName, String sMatch) {
+	public static String getServer(String sMatch) {
 		MatchResult result = null;
 		try {
-		   URL url = new URL(webAddr + fName);
+		   URL url = new URL(MainWindow.webAddress + MainWindow.fileVersion);
 		   Scanner s = new Scanner(url.openStream());
 		   
 		   String text = "";
@@ -33,15 +36,40 @@ public class Version {
 		catch(Exception e) {
 		   ErrorLog.saveError(e);
 		}
-		if (result == null) { return null; }
+		if (result == null) { return "0"; }
 		return result.group(1);
 	}
 	
-	public static String getLocal(String fName) {
+	public static void writeLocal() {
+		try {
+			File config = new File(String.format("%s\\%s", MainWindow.rootDir, MainWindow.fileConfig));
+			
+			BufferedWriter bufferWriter = new BufferedWriter(new FileWriter(config));
+			
+			bufferWriter.write("[Geral]");
+			bufferWriter.newLine();
+			bufferWriter.write(String.format("engine=%s", MainWindow.gameEngine));
+			bufferWriter.newLine();
+			bufferWriter.write(String.format("version=%s", MainWindow.localVer));
+			bufferWriter.newLine();
+			bufferWriter.write(String.format("language=%s", MainWindow.launcherLanguage));
+			bufferWriter.newLine();
+			
+			bufferWriter.close();
+			
+		} catch (Exception e) {
+			ErrorLog.saveError(e);
+		}
+	}
+	
+	public static String getLocal() {
+		
+		if (!new File(String.format("%s\\%s", MainWindow.rootDir, MainWindow.fileConfig)).isFile()) { writeLocal(); return "0"; }
+		
 		MatchResult result = null;
 		
 	    try {
-	    	File config = new File(String.format("%s\\%s", MainWindow.rootDir, fName));
+	    	File config = new File(String.format("%s\\%s", MainWindow.rootDir, MainWindow.fileConfig));
 	    	
 	        Scanner s = new Scanner(config);
 
@@ -61,7 +89,7 @@ public class Version {
 	    } catch (Exception e) {
 	    	ErrorLog.saveError(e);
 	    }
-		if (result == null) { return null; }
+		if (result == null) { return "0"; }
 		return result.group(1);
 		
 	}
